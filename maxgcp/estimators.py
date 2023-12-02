2729e0a59708b36c6a26a3b0e24aa0ca03f131e2
zietzm <michael.zietz@gmail.com>
2023-12-01 19:51:15 -0600

    Update loss function arguments
    Add version to init

--- maxgcp/estimators.py
+++ maxgcp/estimators.py
@@ -1,8 +1,10 @@
-import jax
-import jax.numpy as jnp
-import numpy as np
-import optax
-import pandas as pd
+import functools
+
+import jax  # type: ignore
+import jax.numpy as jnp  # type: ignore
+import numpy as np  # type: ignore
+import optax  # type: ignore
+import pandas as pd  # type: ignore
 
 from maxgcp.loss_functions import (
     genetic_loss_mapper,
@@ -18,41 +20,70 @@ from maxgcp.loss_functions import (
 
 
 class Estimator:
-    def __init__(self):
-        self.n_features = None
+    def __init__(self, cov_G_X, cov_P_X):
+        self.cov_G_X = cov_G_X
+        self.cov_P_X = cov_P_X
+        self.n_features = cov_G_X.shape[0]
         self.coef = None
         self._log_records = list()
+        self.opt_state = None
 
     @property
     def log_df(self):
         return pd.DataFrame(self._log_records)
+
+    @property
+    def coefficients(self) -> np.ndarray:
+        if self.coef is None:
+            raise ValueError("Model has not been trained yet")
+
+        return np.asarray(self.coef)
 
     def predict(self, X):
         return X @ self.coef
 
 
 class SingleGeneticEstimator(Estimator):
-    def _log_iteration(self, step, coef, h2_target, gamma, Cx, Px, print_iter=False):
+    def __init__(self, cov_G_X, cov_P_X):
+        super().__init__(cov_G_X, cov_P_X)
+
+        # Initialize coefficients to 1e-4
+        self.coef = 1e-4 * jnp.ones((self.n_features, 1))
+
+        # Set up loss function
+        self.loss_fn = functools.partial(
+            genetic_loss_vector,
+            cov_G_X=cov_G_X,
+            cov_P_X=cov_P_X,
+        )
+
+        # Gradient of loss function
+        self.loss_grad = jax.grad(self.loss_fn)
+
+    def _log_iteration(self, step, coef, h2_target, gamma, print_iter=False):
         if print_iter:
-            log_function(step, coef, gamma, Cx, Px, h2_target)
+            log_function(step, coef, gamma, self.cov_G_X, self.cov_P_X, h2_target)
 
         self._log_records.append(
             {
                 "step": step,
-                "loss": np.asarray(genetic_loss_vector(coef, gamma, Cx, Px, h2_target)),
-                "h2": np.asarray(h2_vec(coef, Cx, Px)),
-                "rg": np.asarray(rg_vec(coef, gamma, Cx, h2_target)),
+                "loss": np.asarray(
+                    genetic_loss_vector(
+                        coef, gamma, self.cov_G_X, self.cov_P_X, h2_target
+                    )
+                ),
+                "h2": np.asarray(h2_vec(coef, self.cov_G_X, self.cov_P_X)),
+                "rg": np.asarray(rg_vec(coef, gamma, self.cov_G_X, h2_target)),
             }
         )
 
     def train(
         self,
-        h2_target,
-        gamma,
-        Cx,
-        Px,
+        var_G_z,
+        cov_G_z_X,
         n_iter=1000,
         learning_rate=0.001,
+        reset_state=True,
         verbose=True,
         n_log=100,
     ):
@@ -61,105 +92,130 @@ class SingleGeneticEstimator(Estimator):
 
         Parameters
         ----------
-        h2_target : float
-            Target heritability
-        gamma : jax.numpy.ndarray
-            Vector of genetic covariances between each feature and the target (Shape: mx1)
-        Cx : jax.numpy.ndarray
-            Matrix of genetic covariances between each feature (Shape: mxm)
-        Px : jax.numpy.ndarray
-            Matrix of phenotypic covariances between each feature (Shape: mxm)
+        var_G_z : float
+            Genetic variance of the target phenotype
+        cov_G_z_X : jax.numpy.ndarray
+            Vector of genetic covariances between features and the target (Shape: mx1)
         n_iter : int, optional
             Number of training iterations, by default 1000
         learning_rate : float, optional
             Learning rate for Adam optimizer, by default 0.001
+        reset_state : bool, optional
+            Whether to reset the optimizer state, by default True. If False, and the
+            model has been trained before, the previous optimizer state will be used to
+            continue training. Care should be taken when setting this to False, as the
+            optimizer state may not apply to the current target phenotype.
         verbose : bool, optional
             Whether to print during training, by default True
         n_log : int, optional
             How often to log loss, heritability, and genetic correlation, by default
             100 steps
         """
-        self.n_features = Cx.shape[0]
-        self.coef = 1e-4 * jnp.ones((self.n_features, 1))
-
-        genetic_loss_gradient = jax.grad(genetic_loss_vector)
         optimizer = optax.adam(learning_rate=learning_rate)
-        opt_state = optimizer.init(self.coef)
+
+        if reset_state or self.opt_state is None:
+            self.opt_state = optimizer.init(self.coef)
 
         for i in range(n_iter):
             if i % n_log == 0:
-                self._log_iteration(i, self.coef, h2_target, gamma, Cx, Px, verbose)
+                self._log_iteration(i, self.coef, var_G_z, cov_G_z_X, verbose)
 
-            grads = genetic_loss_gradient(self.coef, gamma, Cx, Px, h2_target)
-            updates, opt_state = optimizer.update(grads, opt_state)
+            grads = self.loss_grad(self.coef, cov_G_z_X, var_G_z)
+            updates, self.opt_state = optimizer.update(grads, self.opt_state)
             self.coef = optax.apply_updates(self.coef, updates)
 
         # Log at the end
-        self._log_iteration(i, self.coef, h2_target, gamma, Cx, Px, verbose)
+        self._log_iteration(n_iter, self.coef, var_G_z, cov_G_z_X, verbose)
         return
 
 
 class AllGeneticEstimator(Estimator):
-    def _log_iteration(self, step, coef_mat, gamma_mat, C, P, h2_vec, print_iter=False):
+    def __init__(self, cov_G_X, cov_P_X):
+        super().__init__(cov_G_X, cov_P_X)
+
+        # Create normalizer to zero coefficients on the diagonal
+        self.normalizer = 1 - jnp.eye(self.n_features)
+
+        # Initialize coefficients to 1e-4, but zero out the diagonal
+        self.coef = 1e-4 * self.normalizer
+
+        # Genetic covariance between features and targets (simply ignores self terms)
+        self.cov_G_Z_X = self.normalizer * cov_G_X
+
+        # Set up loss function
+        self.loss_fn = functools.partial(
+            genetic_loss_mean_mapper,
+            cov_G_Z_X=self.cov_G_Z_X,
+            cov_G_X=cov_G_X,
+            cov_P_X=cov_P_X,
+        )
+
+        # Gradient of loss function
+        self.loss_grad = jax.grad(self.loss_fn)
+
+    def _log_iteration(self, step, beta_mat, print_iter=False):
         if print_iter:
-            log_function_mapper(step, coef_mat, gamma_mat, C, P, h2_vec)
+            log_function_mapper(
+                step, beta_mat, self.cov_G_Z_X, self.cov_G_X, self.cov_P_X
+            )
 
         self._log_records.extend(
             [
-                {"step": step, "phenotype": p, "loss": l, "h2": h, "rg": r}
-                for p, l, h, r in zip(
+                {"step": step, "phenotype": p, "loss": loss, "h2": h, "rg": r}
+                for p, loss, h, r in zip(
                     range(self.n_features),
-                    np.asarray(genetic_loss_mapper(coef_mat, gamma_mat, C, P, h2_vec)),
-                    np.asarray(h2_mapper(coef_mat, C, P)),
-                    np.asarray(rg_mapper(coef_mat, gamma_mat, C, h2_vec)),
+                    np.asarray(
+                        genetic_loss_mapper(
+                            beta_mat, self.cov_G_Z_X, self.cov_G_X, self.cov_P_X
+                        )
+                    ),
+                    np.asarray(h2_mapper(beta_mat, self.cov_G_X, self.cov_P_X)),
+                    np.asarray(rg_mapper(beta_mat, self.cov_G_Z_X, self.cov_G_X)),
                 )
             ]
         )
 
-    def train(self, C, P, n_iter=1000, learning_rate=0.001, verbose=True, n_log=100):
+    def train(
+        self,
+        n_iter=1000,
+        learning_rate=1e-5,
+        reset_state=False,
+        verbose=True,
+        n_log=100,
+    ):
         """
-        Train a model to predict genetic liabilities for all traits
+        Train a model to infer the genetic component of each phenotype
 
         Parameters
         ----------
-        C : jax.numpy.ndarray
-            Matrix of genetic covariances (Shape: mxm)
-        P : jax.numpy.ndarray
-            Matrix of phenotypic covariances (Shape: mxm)
         n_iter : int, optional
             Number of training iterations, by default 1000
         learning_rate : float, optional
             Learning rate for Adam optimizer, by default 0.001
+        reset_state : bool, optional
+            Whether to reset the optimizer state, by default False. If False, and the
+            model has been trained before, the previous optimizer state will be used to
+            continue training.
         verbose : bool, optional
             Whether to print during training, by default True
         n_log : int, optional
             How often to log loss, heritability, and genetic correlation, by default
             100 steps
         """
-        self.n_features = C.shape[0]
-
-        # Create normalizer to zero coefficients on the diagonal
-        normalizer = 1 - jnp.eye(self.n_features)
-        self.coef = 1e-4 * normalizer
-
-        # Create matrix and vector needed for training.
-        # Gamma is the matrix of feature-to-target genetic covariance vectors
-        # h2v is the vector of feature heritabilities
-        gam = normalizer * C
-        h2v = jnp.diag(C)
-
-        genetic_loss_gradient = jax.grad(genetic_loss_mean_mapper)
         optimizer = optax.adam(learning_rate=learning_rate)
-        opt_state = optimizer.init(self.coef)
+
+        if reset_state or self.opt_state is None:
+            self.opt_state = optimizer.init(self.coef)
 
         for i in range(n_iter):
             if i % n_log == 0:
-                self._log_iteration(i, self.coef, gam, C, P, h2v, verbose)
+                self._log_iteration(i, self.coef, verbose)
 
-            grads = genetic_loss_gradient(self.coef, gam, C, P, h2v)
-            updates, opt_state = optimizer.update(grads * normalizer, opt_state)
+            # Compute and normalize gradients
+            grads = self.loss_grad(self.coef) * self.normalizer
+            updates, self.opt_state = optimizer.update(grads, self.opt_state)
             self.coef = optax.apply_updates(self.coef, updates)
 
         # Log at the end
-        self._log_iteration(i, self.coef, gam, C, P, h2v, verbose)
+        self._log_iteration(n_iter, self.coef, verbose)
         return
--- maxgcp/loss_functions.py
+++ maxgcp/loss_functions.py
@@ -1,70 +1,71 @@
-import jax
-import jax.numpy as jnp
-from jax import jit
+import jax  # type: ignore
+import jax.numpy as jnp  # type: ignore
+from jax import Array, jit
 
 
 @jit
-def genetic_loss_vector(beta, gamma, C, P, h2_target):
-    target_g_var = beta.T @ C @ beta
-    target_p_var = beta.T @ P @ beta
-    h2 = target_g_var / target_p_var
-    rg = (beta.T @ gamma) / jnp.sqrt(h2_target * target_g_var)
-    return jnp.abs(1 - rg) + jnp.abs(1 - h2)
+def genetic_loss_vector(
+    beta: Array, cov_G_z_X: Array, cov_G_X: Array, cov_P_X: Array, var_G_z: Array
+) -> Array:
+    h2_term = (beta.T @ cov_G_X @ beta) / (beta.T @ cov_P_X @ beta)
+    rg_term = (beta.T @ cov_G_z_X) / jnp.sqrt(var_G_z * beta.T @ cov_G_X @ beta)
+    return jnp.abs(1 - rg_term) + jnp.abs(1 - h2_term)
 
 
 @jit
-def genetic_loss_mean_mapper(coefficient_matrix, gamma_matrix, C, P, h2_vector):
+def genetic_loss_mean_mapper(coefficient_matrix, cov_G_Z_X, cov_G_X, cov_P_X) -> Array:
     return jax.vmap(genetic_loss_vector, (1, 1, None, None, 0))(
-        coefficient_matrix, gamma_matrix, C, P, h2_vector
+        coefficient_matrix, cov_G_Z_X, cov_G_X, cov_P_X, jnp.diag(cov_G_X)
     ).mean()
 
 
-def genetic_loss_mapper(coefficient_matrix, gamma_matrix, C, P, h2_vector):
+def genetic_loss_mapper(coefficient_matrix, cov_G_Z_X, cov_G_X, cov_P_X) -> Array:
     return jax.vmap(genetic_loss_vector, (1, 1, None, None, 0))(
-        coefficient_matrix, gamma_matrix, C, P, h2_vector
+        coefficient_matrix, cov_G_Z_X, cov_G_X, cov_P_X, jnp.diag(cov_G_X)
     )
 
 
-def h2_vec(beta, C, P):
-    target_g_var = beta.T @ C @ beta
-    target_p_var = beta.T @ P @ beta
-    h2 = target_g_var / target_p_var
-    return h2
+def h2_vec(beta, cov_G_X, cov_P_X):
+    # Var_G(y) = sum_{i,j} beta_i beta_j Cov(g_i, g_j)
+    # Var_P(y) = sum_{i,j} beta_i beta_j Cov(x_i, x_j)
+    # h2(y) = Var_G(y) / Var_P(y)
+    return (beta.T @ cov_G_X @ beta) / (beta.T @ cov_P_X @ beta)
 
 
-def rg_vec(beta, gamma, C, h2_target):
-    target_g_var = beta.T @ C @ beta
-    g_cov = beta.T @ gamma
-    rg = g_cov / jnp.sqrt(h2_target * target_g_var)
-    return rg
+def rg_vec(beta, cov_G_z_X, cov_G_X, g_var_z):
+    # Var_G(y) = sum_{i,j} beta_i beta_j Cov(g_i, g_j)
+    # Var_G(z) = g_var_z
+    # Cov_G(z, y) = sum_i beta_i Cov(g_i, g_z)
+    # rg(z, y) = Cov_G(z, y) / sqrt(Var_G(z) * Var_G(y))
+    return (beta.T @ cov_G_z_X) / jnp.sqrt((beta.T @ cov_G_X @ beta) * g_var_z)
 
 
-def h2_mapper(coefficient_matrix, C, P):
-    return jax.vmap(h2_vec, (1, None, None))(coefficient_matrix, C, P)
+def h2_mapper(beta_matrix, cov_G_X, cov_P_X):
+    return jax.vmap(h2_vec, (1, None, None))(beta_matrix, cov_G_X, cov_P_X)
 
 
-def rg_mapper(coefficient_matrix, gamma_matrix, C, h2_vector):
+def rg_mapper(beta_matrix, cov_G_Z_X, cov_G_X):
     return jax.vmap(rg_vec, (1, 1, None, 0))(
-        coefficient_matrix, gamma_matrix, C, h2_vector
+        beta_matrix, cov_G_Z_X, cov_G_X, jnp.diag(cov_G_X)
     )
 
 
-def log_function(step, coefficient_vec, gamma_vec, Cx, Px, h2_target):
-    genetic_loss = genetic_loss_vector(coefficient_vec, gamma_vec, Cx, Px, h2_target)
-    h2 = h2_vec(coefficient_vec, Cx, Px)
-    rg = rg_vec(coefficient_vec, gamma_vec, Cx, h2_target)
+def log_function(step, beta, cov_G_z_X, cov_G_X, cov_P_X, g_var_z):
+    genetic_loss = genetic_loss_vector(beta, cov_G_z_X, cov_G_X, cov_P_X, g_var_z)
+    h2 = h2_vec(beta, cov_G_X, cov_P_X)
+    rg = rg_vec(beta, cov_G_z_X, cov_G_X, g_var_z)
     print(
         f"Step: {step:<5}\tMean loss: {genetic_loss:.5f}\tMean h2: {h2:.4f}\tMean rg: "
         f"{rg:.4f}"
     )
 
 
-def log_function_mapper(step, coefficient_matrix, gamma_matrix, C, P, h2_vector):
+def log_function_mapper(step, beta_matrix, cov_G_Z_X, cov_G_X, cov_P_X):
     genetic_loss_mean = genetic_loss_mean_mapper(
-        coefficient_matrix, gamma_matrix, C, P, h2_vector
+        beta_matrix, cov_G_Z_X, cov_G_X, cov_P_X
     ).item()
-    h2_mean = h2_mapper(coefficient_matrix, C, P).mean().item()
-    rg_mean = rg_mapper(coefficient_matrix, gamma_matrix, C, h2_vector).mean().item()
+    h2_mean = h2_mapper(beta_matrix, cov_G_X, cov_P_X).mean().item()
+    rg_mean = rg_mapper(beta_matrix, cov_G_Z_X, cov_G_X).mean().item()
     print(
         f"Step: {step:<5}\tMean loss: {genetic_loss_mean:.5f}\tMean h2: {h2_mean:.4f}\t"
         f"Mean rg: {rg_mean:.4f}"
