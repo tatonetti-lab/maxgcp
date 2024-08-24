756adca7567f0956a07263b1d6ca6b97a79474d1
zietzm <michael.zietz@gmail.com>
2024-08-23 17:39:21 -0700

    feat: add phenotypic covariance computation

--- src/maxgcp/cli.py
+++ src/maxgcp/cli.py
@@ -22,3 +22,80 @@ app = typer.Typer(
     add_completion=False, context_settings={"help_option_names": ["-h", "--help"]}
 )
 
+
+@app.command(name="pheno-cov")
+def compute_phenotypic_covariance(
+    phenotype_file: Annotated[
+        Path,
+        typer.Option(
+            "--pheno", exists=True, help="Path to phenotype file", show_default=False
+        ),
+    ],
+    output_file: Annotated[
+        Path,
+        typer.Option("--out", help="Path to output file", show_default=False),
+    ],
+    covariate_file: Annotated[
+        Optional[Path],
+        typer.Option("--covar", exists=True, help="Path to covariate file"),
+    ] = None,
+    person_id_col: Annotated[
+        list[str], typer.Option("--person-id", help="Person ID column(s)")
+    ] = ["#FID", "IID"],
+    no_intercept: Annotated[
+        bool, typer.Option("--no-intercept", help="Do not add intercept to covariates")
+    ] = False,
+) -> None:
+    """Compute phenotypic covariance matrix from a phenotype file.
+
+    Covariates are optional. When provided, the covariates will be residualized
+    out and the resulting file will contain the partial covariance matrix.
+
+    Person ID columns will be used to join the phenotype and covariate files and
+    will be ignored in the computation of the covariance matrix. Multiple ID
+    can be specified one or more times like this: --person-id FID --person-id IID
+    """
+    add_intercept = not no_intercept
+    logger.info("Computing covariance...")
+    logger.debug(f"Got phenotype file: {phenotype_file}")
+    logger.debug(f"Got output file: {output_file}")
+    logger.debug(f"Got covariate file: {covariate_file}")
+    logger.debug(f"Got person ID column(s): {person_id_col}")
+    logger.debug(f"Got add intercept: {add_intercept}")
+    if covariate_file is None and add_intercept:
+        logger.warning(
+            "You have specified to add an intercept to the covariates, but no "
+            "covariate file was provided. No intercept will be added."
+        )
+    sep = "," if phenotype_file.suffix == ".csv" else "\t"
+    phenotype_df = pl.read_csv(phenotype_file, separator=sep)
+    has_person_ids = person_id_col is not None and len(person_id_col) > 0
+    if has_person_ids:
+        phenotype_names = phenotype_df.drop(person_id_col).columns
+    else:
+        phenotype_names = phenotype_df.columns
+    if covariate_file is not None:
+        sep = "," if covariate_file.suffix == ".csv" else "\t"
+        covariate_df = pl.read_csv(covariate_file, separator="\t")
+        if add_intercept:
+            covariate_df = covariate_df.with_columns(
+                pl.lit(1.0).alias("const").cast(pl.Float64)
+            )
+        has_person_ids = person_id_col is not None and len(person_id_col) > 0
+        if has_person_ids:
+            covariate_names = covariate_df.drop(person_id_col).columns
+        else:
+            covariate_names = covariate_df.columns
+        merged_df = phenotype_df.join(covariate_df, on=person_id_col)
+        X = merged_df.select(covariate_names).to_numpy()
+        Y = merged_df.select(phenotype_names).to_numpy()
+        beta, _, _, _ = np.linalg.lstsq(X, Y, rcond=None)
+        Y_resid = Y - X @ beta
+        covariance = np.cov(Y_resid.T)
+    else:
+        X = phenotype_df.select(phenotype_names).to_numpy()
+        covariance = np.cov(X.T)
+
+    index = pd.Index(phenotype_names, name="phenotype")
+    covariance_df = pd.DataFrame(covariance, index=index, columns=index)
+    covariance_df.to_csv(output_file, sep="\t")
