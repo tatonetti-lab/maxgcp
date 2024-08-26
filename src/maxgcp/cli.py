f54561209bb45a5323794cb835f7a5824e02ecbf
zietzm <michael.zietz@gmail.com>
2024-08-26 16:29:18 -0700

    feat: add ldsc-munge command

--- src/maxgcp/cli.py
+++ src/maxgcp/cli.py
@@ -1,12 +1,16 @@
 import logging
+import tempfile
 from pathlib import Path
 from typing import Annotated, Optional
 
+import ldsc.scripts.ldsc
+import ldsc.scripts.munge_sumstats
 import numpy as np
 import pandas as pd
 import polars as pl
 import typer
 from rich.logging import RichHandler
+from rich.progress import track
 
 
 logging.basicConfig(
@@ -99,3 +103,63 @@ def compute_phenotypic_covariance(
     index = pd.Index(phenotype_names, name="phenotype")
     covariance_df = pd.DataFrame(covariance, index=index, columns=index)
     covariance_df.to_csv(output_file, sep="\t")
+
+
+@app.command(name="ldsc-munge")
+def ldsc_munge(
+    gwas_path: Annotated[
+        Path,
+        typer.Argument(
+            exists=True, help="Path to GWAS summary statistics", show_default=False
+        ),
+    ],
+    output_file: Annotated[
+        Path,
+        typer.Option("--out", help="Path to output file", show_default=False),
+    ],
+    snp_col: Annotated[str, typer.Option("--snp", help="Name of SNP column")] = "ID",
+    a1_col: Annotated[
+        str, typer.Option("--a1", help="Name of effect allele column")
+    ] = "A1",
+    a2_col: Annotated[
+        str, typer.Option("--a2", help="Name of non-effect allele column")
+    ] = "OMITTED",
+    sample_size_col: Annotated[
+        str, typer.Option("--sample-size", help="Name of sample size column")
+    ] = "OBS_CT",
+    p_col: Annotated[str, typer.Option("--p", help="Name of p-value column")] = "P",
+    signed_sumstat_col: Annotated[
+        str,
+        typer.Option(
+            "--signed-sumstat", help="Name of signed sumstat column (e.g. Z, OR)"
+        ),
+    ] = "T_STAT",
+    signed_sumstat_null: Annotated[
+        float,
+        typer.Option(
+            "--signed-sumstat-null", help="Null value for the signed sumstat column"
+        ),
+    ] = 0.0,
+) -> None:
+    """Process a GWAS summary statistics file using LDSC."""
+    args = ldsc.scripts.munge_sumstats.parser.parse_args(
+        [
+            "--sumstats",
+            gwas_path.as_posix(),
+            "--out",
+            output_file.as_posix(),
+            "--snp",
+            snp_col,
+            "--a1",
+            a1_col,
+            "--a2",
+            a2_col,
+            "--N-col",
+            sample_size_col,
+            "--p",
+            p_col,
+            "--signed-sumstats",
+            f"{signed_sumstat_col},{signed_sumstat_null}",
+        ]
+    )
+    ldsc.scripts.munge_sumstats.munge_sumstats(args)
